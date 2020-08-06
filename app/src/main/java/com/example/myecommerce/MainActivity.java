5057074a3667de6b86f6f5c98ab354603bf94a2f
ravi <panditravi217@yahoo.com>
2020-08-07 00:27:03 +0545

    My_wishlist_34

--- app/src/main/java/com/example/myecommerce/MainActivity.java
+++ app/src/main/java/com/example/myecommerce/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity
     private static final int HOME_FRAGMENT = 0;
     private static final int CART_FRAGMENT = 1;
     private static final int ORDERS_FRAGMENT = 2;
+    private static final int WISHLIST_FRAGMENT = 3;
 
     private FrameLayout frameLayout;
     private ImageView actionBarLogo;
@@ -71,7 +72,14 @@ public class MainActivity extends AppCompatActivity
         if(drawer.isDrawerOpen(GravityCompat.START)){
             drawer.closeDrawer(GravityCompat.START);
         }else{
-            super.onBackPressed();
+            if (currentFragment == HOME_FRAGMENT){
+                super.onBackPressed();
+            }else{
+                actionBarLogo.setVisibility(View.VISIBLE);
+                invalidateOptionsMenu();
+                setFragment(new HomeFragment(),HOME_FRAGMENT);
+                navigationView.getMenu().getItem(0).setChecked(true);
+            }
         }
     }
 
@@ -135,7 +143,7 @@ public class MainActivity extends AppCompatActivity
         }else if (id == R.id.nav_my_cart){
             gotoFragment("My Cart", new MyCartFragment(), CART_FRAGMENT);
         }else if (id == R.id.nav_my_wishlist){
-
+            gotoFragment("My Wishlist", new MyWishlistFragment(), WISHLIST_FRAGMENT);
         }else if (id == R.id.nav_my_account){
 
         }else if (id == R.id.nav_sign_out){
